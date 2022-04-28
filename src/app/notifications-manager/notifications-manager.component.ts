6a58e741cd626262318d7babab17962102249a62
Captain Storm <CaptainStorm21@users.noreply.github.com>
2022-04-28 03:06:26 -0700

    step 5 - Converting the notificationsCount property

--- src/app/notifications-manager/notifications-manager.component.ts
+++ src/app/notifications-manager/notifications-manager.component.ts
@@ -1,4 +1,14 @@
+/**
+ * Convert the notificationsCount property in
+ * notifications-manager.component.ts to an Observable
+ * named notificationsCount$. Inject NotificationsService
+ * in the component and assign the service's count$ Observable
+ * to the component's notificationsCount$ variable:
+ */
 import { Component, OnInit } from '@angular/core';
+//step 5
+import { Observable } from 'rxjs';
+import { NotificationsService } from '../services/notifications.service';
 
 @Component({
   selector: 'app-notifications-manager',
@@ -6,10 +16,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./notifications-manager.component.scss']
 })
 export class NotificationsManagerComponent implements OnInit {
-  notificationsCount = 0
-  constructor() { }
+  // step 5 - convert notificationsCount = 0
+  notificationsCount$: Observable<number>;
+
+  //step 5 - add notification service to constructor
+  constructor(private notificationsService: NotificationsService) { }
 
   ngOnInit(): void {
+    //step 5 -
+    this.notificationsCount$ = this.notificationsService.count$;
   }
 
   addNotification() {
