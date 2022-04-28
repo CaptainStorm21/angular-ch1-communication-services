9c4042f648d8787fede781b2558ac54d2b9a0cf8
Captain Storm <CaptainStorm21@users.noreply.github.com>
2022-04-28 03:19:02 -0700

    step 8 - notifications-button.component.ts inject Observable

--- src/app/notifications-button/notifications-button.component.ts
+++ src/app/notifications-button/notifications-button.component.ts
@@ -1,4 +1,14 @@
+/*
+* Now, similarly inject NotificationsService in
+* notifications-button.component.ts,
+*create an Observable named notificationsCount$
+*inside NotificationsButtonComponent,
+*and assign the service's count$ Observable to it:
+*/
 import { Component, OnInit } from '@angular/core';
+//step 8
+import { NotificationsService } from '../services/notifications.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-notifications-button',
@@ -6,10 +16,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./notifications-button.component.scss']
 })
 export class NotificationsButtonComponent implements OnInit {
-
-  constructor() { }
+ //Step 8
+  notificationsCount$: Observable<number>;
+  constructor(private notificationsService: NotificationsService) { }
 
   ngOnInit(): void {
+    //step 8
+    this.notificationsCount$ = this.notificationsService.count$;
   }
 
 }
