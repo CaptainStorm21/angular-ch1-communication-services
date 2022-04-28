d3254949a82008eca53a877dcaf2c1b030b3ab0e
Captain Storm <CaptainStorm21@users.noreply.github.com>
2022-04-28 02:59:33 -0700

    step 3 - created a behaviorSubject

--- src/app/services/notifications.service.ts
+++ src/app/services/notifications.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
-
+//step 3
+import { BehaviorSubject } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class NotificationsService {
-
+  /*
+  *step 3
+   *Notice that the BehaviorSubject is a
+   *private property and we'll only update
+   *it from within the service using a public method later on.
+  */
+  private count: BehaviorSubject<number> = new BehaviorSubject<number>(0);
   constructor() { }
 }
+
